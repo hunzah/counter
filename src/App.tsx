e8653591e95aa512fb436aba334e2d9d8942f3be
Yusup <hunzah9@mail.ru>
2023-04-27 09:23:54 +0300

    add localStorage logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './App.module.css';
 import {Button} from './components/Button/Button';
 import {Scoreboard} from './components/Scoreboard/Scoreboard';
 
 function App() {
+
+    const maxValue = 5
+    const startValue = 0
+
     let [count, setCount] = useState<number>(0)
+
+    useEffect(()=>{localStorage.setItem('countValue', JSON.stringify(count))},[count])
+
+    useEffect(()=>{
+        let valueAsString = localStorage.getItem('countValue')
+        if (valueAsString) {
+            setCount(JSON.parse(valueAsString))
+
+        }
+        },[])
+
+
     const callbackSum = () => {
-        count < 5 && setCount(++count)
+        count < maxValue && setCount(++count)
     }
     const callbackRes = () => {
-        setCount(0)
+        setCount(startValue)
     }
 
-    const disabledSum = count === 6
+    const disabledSum = count === maxValue
 
-    const disabledRes = count === 0
+    const disabledRes = count === startValue
 
 
     return (
         <div className={s.App}>
             <div className={s.wrapper}>
-                <div
-                    className={s.container}><Scoreboard count={count}/>
+                <div className={s.container}>
+                    <Scoreboard count={count} maxValue={maxValue}/>
                 </div>
                 <div className={s.buttonsContainer}>
                     <Button callbackSum={callbackSum} buttonName="inc" disabled={disabledSum}/>
                     <Button callbackRes={callbackRes} buttonName="reset" disabled={disabledRes}/>
-                    <Button callbackRes={callbackRes} buttonName="settings" disabled={disabledRes}/>
+                    {/*<Button callbackRes={callbackRes} buttonName="settings" disabled={disabledRes}/>*/}
                 </div>
 
             </div>
--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -2,12 +2,13 @@ import React, {FC} from 'react';
 import s from './Scoreboard.module.css'
 type PropsType = {
     count:number
+    maxValue:number
 }
 
 export const Scoreboard: FC<PropsType> = (props) => {
-    const {count} = props
+    const {count, maxValue, ...otherProps} = props
     return (
-        <div className={count === 5? `${s.scoreboard} ${s.disabled}`:s.scoreboard}>
+        <div className={count === maxValue? `${s.scoreboard} ${s.disabled}`:s.scoreboard}>
             {count}
         </div>
     );
