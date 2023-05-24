429264c07ca6b7d597f9f8a03007483504488a65
Yusup <hunzah9@mail.ru>
2023-05-24 14:18:44 +0300

    code refactoring

--- src/components/redux/redux-store.ts
+++ src/components/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux"
+import { legacy_createStore as createStore, combineReducers } from "redux"
 import counterReducer from '../Counter-Reducer/counterReducer';
 
 const reducers = combineReducers(
