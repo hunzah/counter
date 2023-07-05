ea087b66736bd144ba477137ca9d75aac4250605
Yusup <hunzah9@mail.ru>
2023-07-05 12:32:09 +0300

    commit

--- src/components/Counter-Reducer/counterReducer.test.ts
+++ src/components/Counter-Reducer/counterReducer.test.ts
@@ -1,41 +1,39 @@
-import counterReducer, {incrementCountAC, resetCountAC, setMaxValueAC} from './counterReducer';
+import counterReducer, {incrementCountAC, resetCountAC, setMaxValueAC, setStartValueAC} from './counterReducer';
 import {ChangeEvent} from 'react';
 
 describe('all counter Reducer tests', () => {
-
+    const startState = {
+        count: 3,
+        startValue: 0,
+        maxValue: 5,
+    };
     test('count should be increment', () => {
 
-        const startState = {
-            count: 0,
-            startValue: 0,
-            maxValue: 5,
-        };
 
         const endStateState = counterReducer(startState, incrementCountAC())
-        expect(endStateState.count).toBe(1)
+        expect(endStateState.count).toBe(4)
     })
     test('count must be reset', () => {
 
-        const startState = {
-            count: 3,
-            startValue: 0,
-            maxValue: 5,
-        };
 
         const endStateState = counterReducer(startState, resetCountAC())
         expect(endStateState.count).toBe(0)
     })
     test('new maxValue should be added', () => {
 
-        const startState = {
-            count: 3,
-            startValue: 0,
-            maxValue: 5,
-        };
         const mockEvent = {
             target: {value: '8'},
         } as ChangeEvent<HTMLInputElement>;
         const endStateState = counterReducer(startState, setMaxValueAC(mockEvent))
         expect(endStateState.maxValue).toBe(8)
+    })
+    test('new startValue should be added', () => {
+
+
+        const mockEvent = {
+            target: {value: '3'},
+        } as ChangeEvent<HTMLInputElement>;
+        const endStateState = counterReducer(startState, setStartValueAC(mockEvent))
+        expect(endStateState.startValue).toBe(3)
     })
 })
